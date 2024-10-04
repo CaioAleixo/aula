b0c76cd29ed762a0afa0fcaedc10b5000940f4e1
Caio.Aleixo <caio.aleixo@iesb.edu.br>
2024-10-03 21:45:17 -0300

    Continuação aula07

--- aula07/src/components/InputEmail.jsx
+++ aula07/src/components/InputEmail.jsx
@@ -1,16 +1,18 @@
-function InputEmail (props) {
+function InputEmail(props) {
     const regras = {
-        required: { value: true, message: "Email é obrigatório"},
-        pattern: { value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/, message: "Formato de email invalido",}
-    }
-    
+      required: { value: true, message: "Email é obrigatorio" },
+      pattern: {
+        value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+        message: "Formato de email invalido ",
+      },
+    };
     return (
-        <>
-            <label htmlFor="email">E-mail</label>
-            <input type="email" {...props.register("email", regras)}/>
-            {props.error && <p>{error.message}</p>}
-        </>
+      <>
+        <label htmlFor="email">Email</label>
+        <input type="email" {...props.register("email", regras)} />
+        {props.error && <p>{props.error.message}</p>}
+      </>
     );
-}
-
-export default InputEmail;
+  }
+  
+  export default InputEmail;
--- aula07/src/components/InputSenha.jsx
+++ aula07/src/components/InputSenha.jsx
@@ -1,16 +1,18 @@
-function InputSenha (props) {
+function InputSenha(props) {
     const regras = {
-        required: { value: true, message: "Senha é obrigatória"},
-        minLength: { value: 6, message: "A senha deve ter, no mínimo, 6 caracteres"}
-    }
-    
+      required: { value: true, message: "Senha é obrigatoria" },
+      minLength: { 
+          value: 6,
+          message: "A senha deve ter no minimo 6 caracteres" 
+      },
+    };
     return (
-        <>
-            <label htmlFor="senha">Senha</label>
-            <input type="password" {...props.register("senha", regras)}/>
-            {error && <p>{error.message}</p>}
-        </>
+      <>
+        <label htmlFor="senha">Senha</label>
+        <input type="password" {...props.register("senha", regras)}/>
+        {props.error && <p>{props.error.message}</p>}
+      </>
     );
-}
-
-export default InputSenha;
+  }
+  
+  export default InputSenha;
